dd853936b59fdc4b109d153060706cce462d43a5
Brian Reilly <breilly@broadinstitute.org>
2019-05-10 14:21:10 -0400

    WIP

--- src/pages/workspaces/workspace/CatalogDatasetModal.js
+++ src/pages/workspaces/workspace/CatalogDatasetModal.js
@@ -17,13 +17,23 @@ const CatalogDatasetModal = ({ onDismiss, workspace }) => {
   const [datasetOwner, setDatasetOwner] = useState('')
   const [institute, setInstitute] = useState([])
   const [indication, setIndication] = useState('')
+  const [primaryDiseaseSite, setPrimaryDiseaseSite] = useState('')
   const [numSubjects, setNumSubjects] = useState(0)
   const [projectName, setProjectName] = useState('')
   const [dataCategory, setDataCategory] = useState([])
   const [dataType, setDataType] = useState([])
+  const [reference, setReference] = useState('')
+  const [dataFileFormats, setDataFileFormats] = useState('')
+  const [technology, setTechnology] = useState('')
+  const [profilingProtocol, setProfilingProtocol] = useState('')
+  const [coverage, setCoverage] = useState('')
   const [dataUseRestriction, setDataUseRestriction] = useState('')
   const [studyDesign, setStudyDesign] = useState('')
+  const [cellType, setCellType] = useState('')
+  const [ethnicity, setEthnicity] = useState('')
+  const [cohortCountry, setCohortCountry] = useState('')
   const [requiresExternalApproval, setRequiresExternalApproval] = useState(false)
+  const [dataAccessInstructions, setDataAccessInstructions] = useState('')
   const [useLimitationOption, setUseLimitationOption] = useState('')
 
   const makeTextInput = (inputLabel, value, onChange, isRequired = false) => h(IdContainer, [
@@ -39,15 +49,15 @@ const CatalogDatasetModal = ({ onDismiss, workspace }) => {
     }, [inputLabel])])
   ])
 
-  const makeIntInput = (inputLabel, min, value, onChange) => h(IdContainer, [
+  const makeIntInput = (inputLabel, min, value, onChange, isRequired = false) => h(IdContainer, [
     id => h(Fragment, [
-      h(FormLabel, { htmlFor: id }, [inputLabel]),
+      h(FormLabel, { htmlFor: id, isRequired }, [inputLabel]),
       h(IntegerInput, { min, value, onChange })
     ])
   ])
 
-  const makeRadioInput = (inputLabel, value, options, onChange) => div([
-    h(FormLabel, [inputLabel]),
+  const makeRadioInput = (inputLabel, value, options, onChange, isRequired = false) => div([
+    h(FormLabel, { isRequired }, [inputLabel]),
     h(Fragment, _.map(({ optLabel, optValue }) => div([h(RadioButton, {
       onChange: () => onChange(optValue),
       checked: value === optValue,
@@ -68,23 +78,23 @@ const CatalogDatasetModal = ({ onDismiss, workspace }) => {
     makeTextInput('Contact Email', contactEmail, setContactEmail, true),
     makeTextInput('Research Institute', institute, setInstitute, true),
     makeTextInput('Cohort Phenotype/Indication', indication, setIndication, true),
-    // Primary Disease Site
-    makeIntInput('No. of Subjects', 0, numSubjects, setNumSubjects),
-    makeTextInput('Project Name', projectName, setProjectName),
-    makeTextInput('Data Category', dataCategory, setDataCategory),
-    makeTextInput('Experimental Strategy', dataType, setDataType),
-    //Genome Reference Version
-    // Data File Formats
-    // Profiling instrument type
-    // Profiling Protocol
-    /// Depth of Sequencing Coverage (Average)
-    makeTextInput('Data Use Limitation', dataUseRestriction, setDataUseRestriction),
-    makeTextInput('Study Design', studyDesign, setStudyDesign),
-    // cell type
-    // Reported Ethnicity
-    // Cohort Country of Origin
+    makeTextInput('Primary Disease Site', primaryDiseaseSite, setPrimaryDiseaseSite),
+    makeIntInput('No. of Subjects', 0, numSubjects, setNumSubjects, true),
+    makeTextInput('Project Name', projectName, setProjectName, true),
+    makeTextInput('Data Category', dataCategory, setDataCategory, true),
+    makeTextInput('Experimental Strategy', dataType, setDataType, true),
+    makeTextInput('Genome Reference Version', reference, setReference),
+    makeTextInput('Data File Formats', dataFileFormats, setDataFileFormats),
+    makeTextInput('Profiling instrument type', technology, setTechnology),
+    makeTextInput('Profiling Protocol', profilingProtocol, setProfilingProtocol),
+    makeTextInput('Depth of Sequencing Coverage (Average)', coverage, setCoverage),
+    makeTextInput('Data Use Limitation', dataUseRestriction, setDataUseRestriction, true),
+    makeTextInput('Study Design', studyDesign, setStudyDesign, true),
+    makeTextInput('Cell Type', cellType, setCellType),
+    makeTextInput('Reported Ethnicity', ethnicity, setEthnicity),
+    makeTextInput('Cohort Country of Origin', cohortCountry, setCohortCountry),
     makeBooleanInput('Requires External Approval', requiresExternalApproval, setRequiresExternalApproval),
-    // Data Access Instructions
+    makeTextInput('Data Access Instructions', dataAccessInstructions, setDataAccessInstructions),
     makeRadioInput('Choose one of the available options to define Data Use Limitations', useLimitationOption, [
       { optValue: 'questionnaire', optLabel: 'Set Data Use Limitations by answering a questionnaire' },
       { optValue: 'orsp', optLabel: 'Retrieve Data Use Limitations from Broad ORSP' },
