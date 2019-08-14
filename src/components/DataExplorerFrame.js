ca1bf722fa3f7f46d53819dbdfdc575bddeb49ba
Louis Golowich <lgolowich@google.com>
2019-08-14 16:24:23 -0700

    Got rid of iframeresizer error

--- src/components/DataExplorerFrame.js
+++ src/components/DataExplorerFrame.js
@@ -1,6 +1,6 @@
 import { iframeResizer } from 'iframe-resizer'
 import _ from 'lodash/fp'
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 import { iframe } from 'react-hyperscript-helpers'
 import datasets from 'src/data/datasets'
 import * as Nav from 'src/libs/nav'
@@ -9,28 +9,30 @@ import * as Nav from 'src/libs/nav'
 const DataExplorerFrame = ({ dataset }) => {
   const elem = useRef()
   const { origin } = _.find({ name: dataset }, datasets)
-
-  useEffect(() => {
-    iframeResizer({
-      onMessage: ({ message: { importDataQueryStr, deQueryStr } }) => {
-        if (importDataQueryStr) {
-          Nav.history.push({
-            pathname: Nav.getPath('import-data'),
-            search: `?${importDataQueryStr}`
-          })
-        } else if (deQueryStr) {
-          // Propagate Data Explorer URL params to app.terra.bio.
-          // Don't call Nav.history.replace(). That will trigger a request and
-          // cause the page to flicker.
-          window.history.replaceState({}, `Data Explorer - ${dataset}`, `#${Nav.history.location.pathname.slice(1)}?${deQueryStr}`)
+  return iframe({
+    src: `${origin}/?embed&${Nav.history.location.search.slice(1)}`,
+    ref: elem,
+    style: { border: 'none' },
+    onLoad: () => {
+      iframeResizer({
+        onMessage: ({ message: { importDataQueryStr, deQueryStr } }) => {
+          if (importDataQueryStr) {
+            Nav.history.push({
+              pathname: Nav.getPath('import-data'),
+              search: `?${importDataQueryStr}`
+            })
+          } else if (deQueryStr) {
+            // Propagate Data Explorer URL params to app.terra.bio.
+            // Don't call Nav.history.replace(). That will trigger a request and
+            // cause the page to flicker.
+            window.history.replaceState({}, `Data Explorer - ${dataset}`, `#${Nav.history.location.pathname.slice(1)}?${deQueryStr}`)
+          }
         }
-      }
-    }, elem.current)
+      }, elem.current)
 
-    return elem.current.iFrameResizer.removeListeners
-  }, [dataset])
-
-  return iframe({ src: `${origin}/?embed&${Nav.history.location.search.slice(1)}`, ref: elem, style: { border: 'none' } })
+      return elem.current.iFrameResizer.removeListeners
+    }
+  })
 }
 
 export default DataExplorerFrame
